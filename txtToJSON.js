73427c361ac9d780a06421bb53a768aa27869905
Yasmin <ym222bs@student.lnu.se>
2019-12-04 14:58:42 +0100

    added filereader objectcreator

--- txtToJSON.js
+++ txtToJSON.js
@@ -1,26 +1,65 @@
 const fs = require('fs')
 const path = require('path')
-const filepath = path.join(__dirname, '/wikipedia/Words/Games/Action_game')
 
 
-const fileReader = (file) => {
-    let data = ''
-    let readStream = fs.createReadStream(file, 'utf8')
-
-    readStream.on('data', chunk => {
-        data += chunk
-    })
-    .on('end', () => {
-       let result = data.replace(/[.]/g, '')
-        .split(/\s/)
-        .reduce((map, word) =>
-        Object.assign(map, {
-            [word]: (map[word]) ? map[word] + 1 : 1,
-      }),
-      {}
-    )
-    return result
+const stringifyArray = (result) => {
+    fs.writeFile('./wordCounts.json', JSON.stringify(result, null, 2), err => {
+        if (err) throw err
+    
+        console.log('Successfully wrote file')
     })
 }
 
-fileReader(filepath)
+
+const iterateDirectory = () => {
+    const arr = []
+    const directoryPath = path.join(__dirname, 'wikipedia/Words')
+    fs.readdir(directoryPath, function (err, files) {
+        if (err) console.log('Unable to scan directory: ' + err)
+
+        files.forEach(async function (file) {
+            let filePathArray = file.split(' ')
+            filePathArray.forEach(filestring => {
+                let eachArticle = {}
+                eachArticle[filestring] = fileReader(path.join(`${directoryPath}/${filestring}`))
+                arr.push(obj)
+            })
+        })
+        stringifyArray(arr)
+    })
+}
+
+
+const fileReader = (file) => {
+    try {
+        const readStream = fs.readFileSync(file, 'utf8')
+        const words = readStream.replace(/[.]/g, '').split(/\s/)
+
+        const frequencyMap = {}
+        words.forEach(w  => {
+            if (!frequencyMap[w]) {
+                frequencyMap[w] = 0
+            }
+            frequencyMap[w] += 1
+        })
+        
+        return frequencyMap
+
+    // Wiie
+    // const map = words.reduce((acc, e) => acc.set(e, (acc.get(e) || 0) + 1), new Map())
+    
+    // let map = new Map([...new Set(words)].map(
+    //     x => [x, words.filter(y => y === x).length]
+    // ))
+    // console.log(map)
+
+    } catch (error) {
+        console.log(error)
+    }
+  
+}
+
+
+// console.log(iterateDirectory())
+
+iterateDirectory()
